483d9bf9d3765e4ad736ee8417f6bf5f5fef0b5f
eidheim <eidheim@gmail.com>
2015-07-22 17:49:26 +0200

    Simpler Token::get_brief_comments.

--- src/Token.cc
+++ src/Token.cc
@@ -59,57 +59,14 @@ std::string clang::Token::get_type() {
   return spelling;
 }
 
-//TODO: use clang_Cursor_getBriefCommentText
 std::string clang::Token::get_brief_comments() {
   std::string comment_string;
-  auto referenced=clang_getCursorReferenced(cx_cursor);
-  auto comment=clang_Cursor_getParsedComment(referenced);
-  if(clang_Comment_getKind(comment)==CXComment_FullComment) {
-    size_t para_c=0;
-    for(unsigned c=0;c<clang_Comment_getNumChildren(comment);c++) {
-      auto child_comment=clang_Comment_getChild(comment, c);
-      if(clang_Comment_getKind(child_comment)==CXComment_Paragraph) {
-        para_c++;
-        if(para_c>=2)
-          break;
-        for(unsigned c=0;c<clang_Comment_getNumChildren(child_comment);c++) {
-          auto grandchild_comment=clang_Comment_getChild(child_comment, c);
-          if(clang_Comment_getKind(grandchild_comment)==CXComment_Text) {
-            auto cxstr=clang_TextComment_getText(grandchild_comment);
-            comment_string+=clang_getCString(cxstr);
-            comment_string+="\n";
-            clang_disposeString(cxstr);
-            size_t dot_position=comment_string.find(".");
-            if(dot_position!=std::string::npos)
-              return comment_string.substr(0, dot_position);
-          }
-          if(clang_Comment_getKind(grandchild_comment)==CXComment_InlineCommand) {
-            auto cxstr=clang_InlineCommandComment_getCommandName(grandchild_comment);
-            if(comment_string.size()>0)
-              comment_string.pop_back();
-            if(clang_InlineCommandComment_getNumArgs(grandchild_comment)==0)
-              comment_string+=clang_getCString(cxstr);
-            clang_disposeString(cxstr);
-            for(unsigned arg_c=0;arg_c<clang_InlineCommandComment_getNumArgs(grandchild_comment);arg_c++) {
-              auto cxstr=clang_InlineCommandComment_getArgText(grandchild_comment, arg_c);
-              if(cxstr.data!=NULL) {
-                if(arg_c>0)
-                  comment_string+=" ";
-                comment_string+=clang_getCString(cxstr);
-                clang_disposeString(cxstr);
-              }
-            }
-          }
-        }
-      }
-      /*cout << "  " << clang_Comment_getKind(child_comment) << ", children: " << clang_Comment_getNumChildren(child_comment) << endl;
-      auto cxstr=clang_FullComment_getAsHTML(child_comment);
-      cout << "  " << clang_getCString(cxstr) << endl;
-      clang_disposeString(cxstr);*/
-    }
-    while(comment_string.size()>0 && (comment_string.back()=='\n' || comment_string.back()==' '))
-      comment_string.pop_back();
+  auto referenced=get_cursor().get_referenced();
+  if(referenced) {
+    auto cxstr=clang_Cursor_getBriefCommentText(referenced.cx_cursor);
+    if(cxstr.data!=NULL)
+      comment_string=clang_getCString(cxstr);
+    clang_disposeString(cxstr);
   }
-  
   return comment_string;
 }
