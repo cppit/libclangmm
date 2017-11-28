044d7b7a267b9db6fd853316cde9a891d7f1551c
eidheim <eidheim@gmail.com>
2017-11-28 11:41:46 +0100

    Fixed tests on Linux

--- tests/code_complete_results_test.cc
+++ tests/code_complete_results_test.cc
@@ -1,13 +1,24 @@
 #include "clangmm.h"
 #include <string>
 #include <cassert>
+#include <regex>
 
 int main() {
   std::string tests_path=LIBCLANGMM_TESTS_PATH;
   std::string path(tests_path+"/case/main.cpp");
 
+  std::vector<std::string> arguments;
+  auto clang_version_string=clangmm::to_string(clang_getClangVersion());
+  const static std::regex clang_version_regex("^[A-Za-z ]+([0-9.]+).*$");
+  std::smatch sm;
+  if(std::regex_match(clang_version_string, sm, clang_version_regex)) {
+    auto clang_version=sm[1].str();
+    arguments.emplace_back("-I/usr/lib/clang/"+clang_version+"/include");
+    arguments.emplace_back("-I/usr/lib64/clang/"+clang_version+"/include"); // For Fedora
+  }
+
   clangmm::Index index(0, 0);
-  clangmm::TranslationUnit tu(index, path, {});
+  clangmm::TranslationUnit tu(index, path, arguments);
 
   std::string buffer="#include <string>\n"
                      "int main(int argc, char *argv[]) {\n"
--- tests/diagnostics_test.cc
+++ tests/diagnostics_test.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include <regex>
 
 using namespace std;
 
@@ -11,7 +12,17 @@ int main() {
 
   clangmm::Index index(0, 0);
 
-  clangmm::TranslationUnit tu(index, path, {});
+  std::vector<std::string> arguments;
+  auto clang_version_string=clangmm::to_string(clang_getClangVersion());
+  const static std::regex clang_version_regex("^[A-Za-z ]+([0-9.]+).*$");
+  std::smatch sm;
+  if(std::regex_match(clang_version_string, sm, clang_version_regex)) {
+    auto clang_version=sm[1].str();
+    arguments.emplace_back("-I/usr/lib/clang/"+clang_version+"/include");
+    arguments.emplace_back("-I/usr/lib64/clang/"+clang_version+"/include"); // For Fedora
+  }
+
+  clangmm::TranslationUnit tu(index, path, arguments);
   
   auto diagnostics=tu.get_diagnostics();
   assert(diagnostics.size()>0);
