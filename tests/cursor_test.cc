2ae7b5db5a0518ebc726197decc0cc80d023e549
eidheim <eidheim@gmail.com>
2018-05-29 08:43:36 +0200

    Fixed cursor_test for Arch Linux and Fedora

--- tests/cursor_test.cc
+++ tests/cursor_test.cc
@@ -1,13 +1,25 @@
 #include "clangmm.h"
 #include <string>
 #include <cassert>
+#include <regex>
 
 int main() {
   std::string tests_path=LIBCLANGMM_TESTS_PATH;
   std::string path(tests_path+"/case/main.cpp");
 
   clangmm::Index index(0, 0);
-  clangmm::TranslationUnit tu(index, path, {});
+  
+  std::vector<std::string> arguments;
+  auto clang_version_string=clangmm::to_string(clang_getClangVersion());
+  const static std::regex clang_version_regex("^[A-Za-z ]+([0-9.]+).*$");
+  std::smatch sm;
+  if(std::regex_match(clang_version_string, sm, clang_version_regex)) {
+    auto clang_version=sm[1].str();
+    arguments.emplace_back("-I/usr/lib/clang/"+clang_version+"/include");
+    arguments.emplace_back("-I/usr/lib64/clang/"+clang_version+"/include"); // For Fedora
+  }
+  
+  clangmm::TranslationUnit tu(index, path, arguments);
 
   auto cursor=tu.get_cursor(path, 103);
 
